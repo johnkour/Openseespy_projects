3c682cada19bbc15aedac266f941073db3b0639a
John Kouretas <64531567+johnkour@users.noreply.github.com>
2020-07-19 23:06:02 +0300

    Add files via upload

--- Basic_Truss.py
+++ Basic_Truss.py
@@ -47,15 +47,15 @@ def aux_function(lst, scalar):
 
     Parameters
     ----------
-    lst    : TYPE
-        DESCRIPTION.
-    scalar : TYPE
-        DESCRIPTION.
+    lst    : LIST OF FLOATS
+        Initial list with the values in the Imperial System.
+    scalar : FLOAT
+        The value used to convert from Imperial to International System.
 
     Returns
     -------
-    result : TYPE
-        DESCRIPTION.
+    result : LIST OF FLOATS
+        The list with the values converted to S.I..
         
     '''
     
@@ -70,81 +70,167 @@ def aux_function(lst, scalar):
 F = [[t * kip  for t in f]  for f in F]
 
 ##############################################################################
-#####                          Main Analysis                             #####
+#####                     Main Analysis' functions                       #####
 ##############################################################################
 
-# Delete existing model.
-ops.wipe()
+def Model_Build(x, y, A, E):
+    '''
+    Description
+    -----------
+    This function is used to determine the basic parameters of the structural
+    problem at hand.
+    
+    Parameters
+    ----------
+    x : LIST OF FLOATS
+        The list of the coordinates of the nodes along the x-axis.
+    y : LIST OF FLOATS
+        The list of the coordinates of the nodes along the y-axis.
+    A : LIST OF FLOATS
+        The list with the materials used for the different elements.
+    E : FLOAT
+        The modulus of elesticity of the elements.
+    Returns
+    -------
+    None.
 
-# Define the model.
-ops.model('basic', '-ndm', 2, '-ndf', 3)
+    '''
+    # Delete existing model.
+    ops.wipe()
+    
+    # Define the model.
+    ops.model('basic', '-ndm', 2, '-ndf', 3)
+    
+    # Define materials.
+    ops.uniaxialMaterial('Elastic', 1, E)
+    
+    # Define the nodes.
+    m = len(x)
+    
+    [ops.node(i + 1, *[x[i], y[i]])  for i in range(m)]
+    
+    # Fix the nodes.
+    fixxity = [[0, 0, 1], [0, 1, 1], [1, 0, 1], [1, 1, 1]]
+    
+    [ops.fix(i + 1, *fixxity[3])  if i + 1 != 4  else ops.fix(i + 1, *fixxity[0])
+                 for i in range(m)]
+    
+    # Define elements.
+    conn = [[1, 4], [2, 4], [3, 4]]
+    [ops.element('Truss', i + 1, *conn[i], A[1], 1)  if i != 0
+                 else  ops.element('Truss', i + 1, *conn[i], A[0], 1)
+                 for i in range(len(conn))]
+    
+    # Plot model.
+    opsplt.plot_model()
+    
+def Rec_Setup(analysis):
+    '''
+    Description
+    -----------
+    This function is used to set up the recorders. It stores the output of the 
+    recorders to a folder, whose name is the value of the variable: analysis.
 
-# Define materials.
-ops.uniaxialMaterial('Elastic', 1, E)
+    Parameters
+    ----------
+    analysis : STRING
+        The name of the analysis, currently performed.
 
-# Define the nodes.
-m = len(x)
+    Returns
+    -------
+    None.
 
-[ops.node(i + 1, *[x[i], y[i]])  for i in range(m)]
+    '''
+    
+    analysis += '/'
+    
+    # Set recorders.
+    ops.recorder('Node', '-file', analysis + 'NodeDisp.txt', '-time', '-node', *[4], '-dof', *[1, 2, 3], 'disp')
+    ops.recorder('Node', '-file', analysis + 'ReactDisp.txt', '-time', '-node', *[4], '-dof', *[1, 2, 3], 'reaction')
+    ops.recorder('Element', '-file', analysis + 'ElementsForces.txt', '-time', '-ele', *[1, 2, 3], 'forces')
+    
 
-# Fix the nodes.
-fixxity = [[0, 0, 1], [0, 1, 1], [1, 0, 1], [1, 1, 1]]
+def Analysis_setup(analysis, F, N = 1):
+    '''
+    Description
+    -----------
+    This functions is used to setup and then run the analysis.
 
-[ops.fix(i + 1, *fixxity[3])  if i + 1 != 4  else ops.fix(i + 1, *fixxity[0])
-             for i in range(m)]
+    Parameters
+    ----------
+    analysis : STRING
+        The name of the analysis, currently performed.
+    F        : LIST OF LISTS OF FLOATS
+        The list containig a list of loads along the x and y axises and around
+        the z axis for every node.
+    N        : INTEGER
+        The number of the analysises to run. Default value: 1
 
-# Define elements.
-conn = [[1, 4], [2, 4], [3, 4]]
-[ops.element('Truss', i + 1, *conn[i], A[1], 1)  if i != 0
-             else  ops.element('Truss', i + 1, *conn[i], A[0], 1)
-             for i in range(len(conn))]
+    Returns
+    -------
+    None.
 
-# Create timeseries.
-ops.timeSeries('Linear', 1)
+    '''
+    
+    # Auxiliary variable.
+    m = len(F)
+    
+    # Create timeseries.
+    ops.timeSeries('Linear', 1)
+    
+    # Create load pattern.
+    ops.pattern('Plain', 1 , 1)
 
-# Create load pattern.
-ops.pattern('Plain', 1 , 1)
+    # Define loads.
+    [ops.load(i + 1, *F[i])  for i in range(m)]
 
-# Define loads.
-[ops.load(i + 1, *F[i])  for i in range(m)]
+    # Define system.
+    ops.system('BandSPD')
+    
+    # Define numberer.
+    ops.numberer('RCM')
+    
+    # Define constraint handler
+    ops.constraints('Plain')
+    
+    # Define integrator.
+    ops.integrator('LoadControl', 1.0)
+    
+    # Define algorithm
+    ops.algorithm('Linear')
+    
+    # Create analysis object
+    ops.analysis('Static')
+    
+    # Execute the analysis
+    ops.initialize()    # Set recorders to start recording at 0 time.
+    
+    ok = ops.analyze(N)
+    
+    if ok == 0:
+        status = 'Analysis complete, everything went smoothly.'
+    else:
+        status = 'ERROR: ANALYSIS FAILED TO CONVERGE' + '\n' + 'Tip: Change algorithm'
+    
+    print(analysis + '\n' + status)
+    
+    # Close recorders and scrap model.
+    ops.wipe()
+    
 
-# Set recorders.
-ops.recorder('Node', '-file', 'NodeDisp.txt', '-time', '-node', *[4], '-dof', *[1, 2, 3], 'disp')
-ops.recorder('Node', '-file', 'ReactDisp.txt', '-time', '-node', *[4], '-dof', *[1, 2, 3], 'reaction')
-ops.recorder('Element', '-file', 'ElementsForces.txt', '-time', '-ele', *[1, 2, 3], 'forces')
+##############################################################################
+#####                            Main Analysis                           #####
+##############################################################################
 
-# Define system.
-ops.system('BandSPD')
+# Step 1: Initilize model parameters.
+Model_Build(x, y, A, E)
 
-# Define numberer.
-ops.numberer('RCM')
+# Step 2: Name the type of the analysis to be performed.
+analysis = 'Static'
 
-# Define constraint handler
-ops.constraints('Plain')
+# Step 3: Set up the recorders.
+Rec_Setup(analysis)
 
-# Define integrator.
-ops.integrator('LoadControl', 1.0)
-
-# Define algorithm
-ops.algorithm('Linear')
-
-# Create analysis object
-ops.analysis('Static')
-
-# Execute the analysis
-ops.initialize()    # Set recorders to start recording at 0 time.
-
-ok = ops.analyze(10)
-
-if ok == 0:
-    status = 'Analysis complete, everything went smoothly.'
-else:
-    status = 'ERROR: ANALYSIS FAILED TO CONVERGE' + '\n' + 'Tip: Change algorithm'
-
-print(status)
-
-# Plot model.
-opsplt.plot_model()
-
-# Close recorders and scrap model.
-ops.wipe()
+# Step 4: Perform the analysis.
+N = 10    # Number of analysises to be performed.
+Analysis_setup(analysis, F, N)
