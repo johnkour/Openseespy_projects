1aab52b8d728f7eefca495fd1f2ac5b56ab9099e
John Kouretas <64531567+johnkour@users.noreply.github.com>
2023-12-05 00:41:02 +0200

    Upload of excersise 9.
    Initial upload of excersise 9.

---
+++ Μη γραμμική ανάλυση των ραβδωτών φορέων/Ασκήσεις/Σειρά 3/Άσκηση 9/plotter.py
@@ -0,0 +1,94 @@
+"""
+A basic program to plot in the same PDF file P - delta curves from text files 
+generated from Openseespy recorders.
+
+@author: John Kouretas
+"""
+
+
+##############################################################################
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+##############################################################################
+
+
+def Plot_Pdelta(F, filename):
+    
+    fig, ax = plt.subplots()
+    
+    ax.set_xlabel('Vertical displacement at 20% of horizontal beam [m]')
+    ax.set_ylabel('Force [MN]')
+    
+    ax.set_title("P-delta curves for Lee's Frame")
+    plt.grid(linestyle='--', linewidth=2)
+    
+    for Analysis in ['Linear', 'PDelta', 'Corotational']:
+        
+        fname = Analysis + '/'
+        fname += filename
+        temp = np.loadtxt(fname, delimiter = ' ')
+        Data = np.column_stack((temp[:, 0], temp[:, 2]))
+        Data = np.insert(Data, [0], [[0, 0]], axis= 0)
+        
+        P = Data[:, 0]
+        P *= F
+        P /= 10**3
+            
+        disp = Data[:, 1]
+        disp *= -1
+        #disp *= 10**2
+        
+        lnstyle = 'solid'
+        if Analysis == 'PDelta':
+            lnstyle = 'dotted'
+        #(0, (5, 10))
+        
+        ax.plot(disp, P, label = Analysis + ' ' + 'analysis', 
+                linestyle = lnstyle)
+        
+    
+    ax.legend(loc = 'upper left')
+    #ax.legend(loc = 'lower right')
+    
+    ax.set_xlim(left = 0, right = 1.1)
+    ax.set_ylim(bottom = -15, top = 50)
+    
+    filename = filename[:-4]
+    filename += '.png'
+    plt.savefig(filename)
+    
+    filename = filename[:-4]
+    filename += '.pdf'
+    plt.savefig(filename)
+    
+    plt.show()
+    
+
+##############################################################################
+
+
+text1 = '''
+Please enter the name of the file containing the results of the analysis (don't write the ".txt"):
+'''
+
+filename = input(text1)
+temp = filename
+filename += '.txt'
+
+text2 = '''
+Please enter the value of the load that was applied when analizing in N (P):
+'''
+
+P = input(text2)
+P = float(P)
+
+Plot_Pdelta(P, filename)
+
+
+##############################################################################
+
+
